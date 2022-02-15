a86013faf08747cdce065d7b5ed309452cc77783
xysong <xysong25@gmail.com>
2022-02-15 03:21:39 -0500

    reboot implementation first stage

--- R scripts/Rasch.R
+++ R scripts/Rasch.R
@@ -23,13 +23,26 @@ extract_model_configuration <- function(N, J, p, model, mc) {
 num_latent_fac <- 1
 num_mc_iter <- 100
 
-config_dict <- extract_model_configuration(100, 10, "Rasch")
+config_dict <- extract_model_configuration(100, 10, 1, "Rasch", 10)
 
 print("Rasch Model Implementation using MIRT package")
 # Ground Truth: small-scale experiments
 a <- matrix(1, 10, 1)
 d <- matrix(c(0, 1, 2, 3, 4, 0, -1, -2, -3, -4), 10, 1)
-# Data is generated using 2-parameter logistic model, where a is fixed to be 1.
+
+# Global parameters
+N = 1000
+J = 10
+p = 1
+# REBOOT algorithm
+reboot_fnorm <- avg_fnorm <- local_fnorm <- rep(0, num_mc_iter) # Store F-norm 
+reboot_est <- avg_est <- local_est <- matrix(0, J, p) # Store est. parameters
+
+for (i in 1:num_mc_iter) {
+  # Generate data
+  # Data is generated using 2-parameter logistic model, where a is fixed to be 1.
+
+}
 data <- simdata(a = a, d = d, N = 100, itemtype = rep("2PL", 10))
 values <- mirt(data, num_latent_fac, method = "MHRM", TOL = 0.0001,
         itemtype = "Rasch", technical = list(MHDRAWS = 1, NCYCLES = 1e5,
@@ -38,10 +51,5 @@ model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
         TOL = 0.0001, technical = list(NCYCLES = 1e5, MHDRAWS = 1,
         gain = c(1, 1)), pars = values)
 
-# Global parameters
-N = 1000
-J = 10
-p = 1
-# REBOOT algorithm
-reboot_fnorm <- avg_fnorm <- matrix(0, N, J) # Store F-norm 
-reboot_est <- avg_est <- matrix(0, N, J) # Store estimated parameters
+coef(model)[[1]][2]
+
