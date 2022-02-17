b35ddbba83796c93b5c795d2f1e2b48f0e985b2d
xysong <xysong25@gmail.com>
2022-02-17 02:06:31 -0500

    functionalize the model fitting procedure

--- R scripts/Rasch.R
+++ R scripts/Rasch.R
@@ -24,6 +24,19 @@ compute_average_estimator <- function(local_est_all, ground_truth) {
   avg_fnorm <- norm((avg_est - ground_truth), "F")
   return(list(est = avg_est, err = avg_fnorm))
 }
+
+fit_mirt <- function(data, num_latent_fac) {
+  values <- mirt(data, num_latent_fac, method = "MHRM", TOL = 0.0001,
+        itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
+        gain = c(1, 1)), pars = "values")
+  # add constraint to parameter matrix.
+  values[2, 6] <- 0
+  values[2, 9] <- FALSE
+  model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
+        TOL = 0.0001, technical = list(NCYCLES = 1e5, MHDRAWS = 5,
+        gain = c(1, 1)), pars = values)
+  return(model)
+}
 # data <- simdata(a = a, d = d, N = 2, itemtype = rep("2PL", 10))
 # sum(a[a < 1]) == 0
 # data
@@ -62,17 +75,18 @@ num_mc_iter <- 2
 
 print("Rasch Model Implementation using MIRT package")
 # Ground Truth: small-scale experiments
-a <- matrix(1, 5, 1)
+a <- matrix(1, 10, 1)
 a
-d <- matrix(c(0, 1, 2, -1, -2), 5, 1)
+d <- matrix(c(0, 0.5, 1, 1.5, 2, 0, -0.5, -1, -1.5, -2), 10, 1)
 d
 # Global parameters
 N = 1000
-J = 5
-m = 50
+J = 10
+m = 10
 p = 1
 R = 10 # Amplification ratio
 n <- N / m # local sample size
+
 # REBOOT algorithm
 reboot_fnorm <- avg_fnorm <- local_fnorm <- cmirt_fnorm <- rep(0, num_mc_iter) # nolint
 reboot_est <- avg_est <- local_est <- cmirt_est <- list() # nolint
@@ -80,18 +94,11 @@ for (i in 1:num_mc_iter) {
   # Generate data
   # Data is generated using 2-parameter logistic model (slope is fixed to be 1).
   data <- simdata(a = a, d = d, N = N, itemtype = rep("2PL", J))
+  # TODO: process the data before fitting the model.
   # Temporary solution: (by introducing noise)
   # noise_index <- sample(1:1000, J, replace = TRUE)
   # Compute centralized MIRT estimator.
-  values <- mirt(data, num_latent_fac, method = "MHRM", TOL = 0.0001,
-        itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
-        gain = c(1, 1)), pars = "values")
-  # add constraint to parameter matrix.
-  values[2, 6] <- 0
-  values[2, 9] <- FALSE
-  model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
-        TOL = 0.0001, technical = list(NCYCLES = 1e5, MHDRAWS = 5,
-        gain = c(1, 1)), pars = values)
+  model <- fit_mirt(data, num_latent_fac)
   # Compute full-sample mirt estimator and fnorm
   cmirt_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
   cmirt_fnorm[[i]] <- norm((cmirt_est[[i]] - d), "F")
@@ -101,15 +108,7 @@ for (i in 1:num_mc_iter) {
     # Split data into m local machine.
     local_data <- data[(1 + (l - 1) * n):(n * l),]
     # Fit the mirt model
-    values <- mirt(local_data, num_latent_fac, method = "MHRM", TOL = 0.0001,
-        itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
-        gain = c(1, 1)), pars = "values")
-    # add constraint to parameter matrix.
-    values[2, 6] <- 0
-    values[2, 9] <- FALSE
-    model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
-        TOL = 0.0001, technical = list(NCYCLES = 1e5, MHDRAWS = 5,
-        gain = c(1, 1)), pars = values)
+    model <- fit_mirt(local_data, num_latent_fac)
     # Extract model coefficients.
     local_est_all[[l]] <- simplify2array(matrix(coef(model, simplify = TRUE)[[1]][, 2])) #nolint
     # Extract local estimator (WLOG, we take the estimator of the first client)
@@ -127,18 +126,12 @@ for (i in 1:num_mc_iter) {
   }
   reboot_data <- as.data.frame(do.call(rbind, reboot_data))
   # Final fit using Reboot data.
-  values <- mirt(reboot_data, num_latent_fac, method = "MHRM", TOL = 0.0001,
-        itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
-        gain = c(1, 1)), pars = "values")
-  # add constraint to parameter matrix.
-  values[2, 6] <- 0
-  values[2, 9] <- FALSE
-  model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
-        TOL = 0.0001, technical = list(NCYCLES = 1e5, MHDRAWS = 5,
-        gain = c(1, 1)), pars = values)
+  model <- fit_mirt(reboot_data, num_latent_fac)
+  # Extract reboot estimator and compute F-norm.
   reboot_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
   reboot_fnorm[[i]] <- norm((reboot_est[[i]] - d), "F")
 }
+
 print("Full-Sample Estimator:")
 print(mean(cmirt_fnorm))
 print("Local Estimator:")
