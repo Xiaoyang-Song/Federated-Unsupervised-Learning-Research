ff0e6b4cc0c72e87c46ed8062c812a93914b921e
xysong <xysong25@gmail.com>
2022-03-07 22:40:18 -0500

    add rasch_naive.R

--- model_implementation/Rasch_model/Rasch_naive.R
+++ model_implementation/Rasch_model/Rasch_naive.R
@@ -18,19 +18,15 @@ model_config <- function(n, j, p, model, mc) {
   ))
 }
 
-fit_mirt <- function(data, num_latent_fac, d = 0, cols_info, CHECK_SINGLE_RESPONSE) {
+fit_mirt <- function(data, num_latent_fac, d = 0) {
   num_cols <- ncol(data) #nolint
   values <- mirt(data, num_latent_fac, method = "MHRM", TOL = 0.0001,
         itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
         gain = c(1, 1)), pars = "values")
+  # TODO: automatic set the constraint based on input data.
   # add identification constraint to parameter matrix.
-  if (CHECK_SINGLE_RESPONSE) {
-    values[2, 6] <- d[which(cols_info > 0)[1]]
-    values[2, 9] <- values[4 * length(which(cols_info > 0)) + 2, 9] <- FALSE
-  } else {
-    values[2, 6] <- d[1]
-    values[2, 9] <- values[42, 9] <- FALSE
-  }
+  values[2, 6] <- 0
+  values[2, 9] <- values[42, 9] <- FALSE
   model <- mirt(data, num_latent_fac, method = "MHRM", itemtype = "Rasch",
         TOL = 0.0001, verbose = FALSE, technical = list(NCYCLES = 1e5, MHDRAWS = 10, #nolint
         gain = c(1, 1)), pars = values)
@@ -51,93 +47,21 @@ get_final_results <- function(cmirt_fnorm, local_fnorm,
 
 check_single_response <- function(data) {
   cols <- list()
-  counter <- 1
   for (col in 1:ncol(data)) {
     if (!any(data[, col] == 1)) {
       cols[[col]] <- -1
     } else if (!any(data[, col] == 0)) {
-      cols[[col]] <- 0 # 0 represents that there is all one
+      cols[[col]] <- 1
     } else {
-      cols[[col]] <- counter
-      counter <- counter + 1
+      cols[[col]] <- 0
     }
   }
-  return(list(cols_info = cols))
+  return(list(cols = cols))
 }
-
-combine_estimation <- function(BOUNDS, cols_info, est, num_latent_fac, J) {
-  #TODO: change the way of passing through BOUNDS information in the future.
-  #      maybe find a way to resolve that outside this function.
-  bound_est <- rep(BOUNDS, num_latent_fac)
-  results <- list()
-  for (idx in 1:J) {
-    if (cols_info[idx] == 0) {
-      results[[idx]] <- bound_est * 1
-    } else if (cols_info[[idx]] == -1) {
-      results[[idx]] <- bound_est * (-1)
-    } else {
-      results[[idx]] <- est[cols_info[[idx]]]
-    }
-  }
-  return(estimator = matrix(results))
-}
-
-get_est <- function(data, num_latent_fac, J, d, CHECK_SINGLE_RESPONSE, BOUNDS) {
-  if (CHECK_SINGLE_RESPONSE) {
-    cols_info <- check_single_response(data)$cols_info
-    data_trim <- data[, (cols_info > 0)]
-    model <- fit_mirt(data = data_trim, num_latent_fac = num_latent_fac, d = d,
-          cols_info = cols_info, CHECK_SINGLE_RESPONSE = CHECK_SINGLE_RESPONSE)
-    partial_estimator <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
-    estimator <- combine_estimation(BOUNDS = BOUNDS, cols_info = cols_info,
-          est = partial_estimator, num_latent_fac = num_latent_fac, J = J)
-  } else {
-    model <- fit_mirt(data = data, num_latent_fac = num_latent_fac, d = d,
-          cols_info = cols_info, CHECK_SINGLE_RESPONSE = CHECK_SINGLE_RESPONSE)
-    estimator <- matrix(coef(model, simplify = TRUE)[[1]][, 2])
-  }
-  return(estimator = estimator)
-}
-# fit_mirt(data_trim, 1, d, col)
-# data <- simdata(a = a, d = d, N = 10, itemtype = rep("2PL", J))
-# col <- check_single_response(data)$cols
-# mask <- col > 0
-# mask
-# data_trim <- data[, mask]
-# data_trim
-# values <- mirt(data_trim, num_latent_fac, method = "MHRM", TOL = 0.0001,
-#         itemtype = "Rasch", technical = list(MHDRAWS = 5, NCYCLES = 1e5,
-#         gain = c(1, 1)), pars = "values")
-# model <- mirt(data_trim, num_latent_fac, method = "MHRM", itemtype = "Rasch",
-#         TOL = 0.0001, verbose = FALSE, technical = list(NCYCLES = 1e5, MHDRAWS = 10, #nolint
-#         gain = c(1, 1)), pars = values)
-# coef(model)
-# coef <- simplify2array(matrix(coef(model, simplify = TRUE)[[1]][, 2]))
-# coef
-# df <- data.frame(coef)
-# df
-# bounds <- rep(1, 1)
-# results <- list()
-# bounds
-# combine_estimation(2, col, coef, 1, 10)
-# d[which(col > 0)[1]]
-# length(which(col > 0))
-# col[col > 0][2]
-# for (idx in 1:10) {
-#   if (col[idx] == 0) {
-#     results[[idx]] <- bounds * 1
-#   } else if (col[[idx]] == -1) {
-#     results[[idx]] <- bounds * (-1)
-#   } else {
-#     results[[idx]] <- coef[col[[idx]]]
-#   }
-# }
-# matrix(results)
 
 # TODO: implement a function to automatically check whether
 #       we need to check single response based on probabilities.
 # Simulation study setup
-set.seed(2022)
 num_mc_iter <- 2 # Ground Truth: small-scale experiments
 a <- matrix(1, 10, 1)
 a
@@ -151,6 +75,7 @@ d
 #TODO: Implementing the pipeline for all models.
 # Model specification
 # For Rasch model, there is only one latent factor to extract.
+set.seed(2022)
 num_latent_fac <- 1
 print("Rasch Model Implementation using MIRT package")
 # Global parameters
@@ -171,11 +96,9 @@ for (i in 1:num_mc_iter) {
   # Data is generated using 2-parameter logistic model (slope is fixed to be 1).
   data <- simdata(a = a, d = d, N = N, itemtype = rep("2PL", J))
   # Compute centralized MIRT estimator.
-  # model <- fit_mirt(data, num_latent_fac)
+  model <- fit_mirt(data, num_latent_fac)
   # Compute full-sample mirt estimator and fnorm
-  cmirt_est[[i]] <- simplify2array(get_est(data, num_latent_fac, J,
-                            d, CHECK_SINGLE_RESPONSE, EST_BOUND))
-  # cmirt_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
+  cmirt_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
   cmirt_fnorm[[i]] <- norm((cmirt_est[[i]] - d), "F")
   # Start distributed setting.
   local_est_all <- list()
@@ -183,12 +106,15 @@ for (i in 1:num_mc_iter) {
     print(paste("MIRT on Machine ", l))
     # Evenly split data into m local machine.
     local_data <- data[(1 + (l - 1) * n):(n * l),]
+    # TODO: Add boolean logic to check whether need to check single response.
+    #       This is required for fast computation when running large scale exps.
+    # TODO: If CHECK_SINGLE_RESPONSE is true, process the data before fit_mirt.
+    # TODO: Set all "singular" items to +/- EST_BOUND based on its response.
     # Fit the mirt model
-    # model <- fit_mirt(local_data, num_latent_fac) #nolint
+    model <- fit_mirt(local_data, num_latent_fac)
+    # TODO: After fit_mirt, re-combine all estimation together.
     # Extract model coefficients.
-    # local_est_all[[l]] <- simplify2array(matrix(coef(model, simplify = TRUE)[[1]][, 2])) #nolint
-    local_est_all[[l]] <- simplify2array(get_est(local_data, num_latent_fac, J,
-                            d, CHECK_SINGLE_RESPONSE, EST_BOUND))
+    local_est_all[[l]] <- simplify2array(matrix(coef(model, simplify = TRUE)[[1]][, 2])) #nolint
     # Extract local estimator (WLOG, we take the estimator of the first client)
     if (l == 1) {
       local_est[[i]] <- local_est_all[[l]]
@@ -196,7 +122,7 @@ for (i in 1:num_mc_iter) {
     }
   }
   # Compute average estimators and errors.
-  avg_est[[i]] <- rowMeans(simplify2array(local_est_all))
+  avg_est[[i]] <- apply(simplify2array(local_est_all), 1:2, mean)
   avg_fnorm[[i]] <- norm((avg_est[[i]] - d), "F")
   # Generate Bootstrap samples.
   reboot_data <- list()
@@ -205,10 +131,9 @@ for (i in 1:num_mc_iter) {
   }
   reboot_data <- as.data.frame(do.call(rbind, reboot_data))
   # Final fit using Reboot data.
+  model <- fit_mirt(reboot_data, num_latent_fac)
   # Extract reboot estimator and compute F-norm.
-  # reboot_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
-  reboot_est[[i]] <- simplify2array(get_est(reboot_data, num_latent_fac, J,
-                            d, CHECK_SINGLE_RESPONSE, EST_BOUND))
+  reboot_est[[i]] <- matrix(coef(model, simplify = TRUE)[[1]][, 2]) #nolint
   reboot_fnorm[[i]] <- norm((reboot_est[[i]] - d), "F")
 }
 get_final_results(cmirt_fnorm, local_fnorm, avg_fnorm, reboot_fnorm)
