10028d83759eb905502690fa4db0e461f3362c1a
xysong <xysong25@gmail.com>
2022-04-14 00:40:32 -0400

    nlm optimization of sur_loss function

--- models/Rasch model/mle.R
+++ models/Rasch model/mle.R
@@ -1,18 +1,25 @@
-# Arthor: Xiaoyang Song
-library(bspec)
+library(stats4)
+library(mirt)
 
-x <- rnorm(10, 1, 1)
-# print(x * density(x, n = 10)$y) #nolint
-# print(x)
-# print(sum(dnorm(x)))
-# print(sum(x * dnorm(x)))
-# print(mean(x < 0))
-y <- 1
-fn <- function(d) {
-  return(integrate(ex, - Inf, Inf)$value)
-}
-ex <- function(x) {
-  return((-y * (x - d) + log(1 + exp(x - d))) * dnorm(x))
-}
+N <- 100
+J <- 10
+a <- matrix(1, 10, 1)
+est_x_ratio <- 1000
+d_star <- matrix(c(0, -0.5, 0.1, -0.4, 0.2, -0.3, 0.3, -0.2, 0.4, -0.1), 10, 1)
 
-print(nlm(fn, d))
+Y <- simdata(a = a, d = d_star, N = N, itemtype = rep("2PL", J))
+
+nll <- function(d) {
+  loss <- 0
+  for (i in 1:N) {
+    for (j in 1:J) {
+      x <- rnorm(est_x_ratio, 0, 1)
+      loss <- loss + mean(-Y[i, j] * (x - d[j]) + log(1 + exp(x - d[j])))
+    }
+  }
+  return(loss)
+}
+# the vector below are starting values of estimation
+# nlm() uses Newton-type minimization algorithm.
+nlm_obj <- nlm(nll, c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+print(nlm_obj)
