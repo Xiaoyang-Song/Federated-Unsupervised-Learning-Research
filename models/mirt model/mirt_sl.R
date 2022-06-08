a922dc555fa762dee03d616794a09caa6bde8465
xysong <xysong25@gmail.com>
2022-06-08 13:43:45 -0400

    Fix two bugs in mirt_sl.R

--- models/mirt model/mirt_sl.R
+++ models/mirt model/mirt_sl.R
@@ -2,10 +2,24 @@ library(mirt)
 library(Rlab)
 library(MASS)
 # set.seed(2022)
+qr_decomp <- function(target) {
+  QRdecomp <- qr(target)
+  R <- qr.R(QRdecomp)
+  Q <- qr.Q(QRdecomp)
+  sgn <- sign(diag(R))
+  R <- diag(sgn) %*% R
+  Q <- Q %*% diag(sgn)
+  return(list(R = t(R), Q = Q))
+}
+sinTheta <- function(A1, A2) {
+  V1 <- svd(A1)$u
+  V2 <- svd(A2)$u
+  return(norm((V1 %*% t(V1) - V2 %*% t(V2)), "F"))
+}
 mse <- function(x) {
   return(sum(x ^ 2))
 }
-l2 <- function(x) {
+fnorm <- function(x) {
   return(sum(x ^ 2) ^ 0.5)
 }
 Y2Ytilde <- function(Y) {
@@ -18,14 +32,18 @@ Y2Ytilde <- function(Y) {
 
 J <- 5
 K <- 2
-N <- 200
-R <- 10
+N <- 2000
+R <- 100
 M <- N * R
+mc <- 1
 error_surr <- rep(0, 100)
 error_mirt <- rep(0, 100)
 # For now, there is no intercept term involved.
 a <- matrix(c(2.2, 2.0, 2.6, -1.4, -1.2, 0, 0, 0, 3.2, 1.7), J, K) # J x K #nolint
 a
+a_2d <- matrix(t(a), byrow = TRUE, nrow = J)
+a_2d
+# fnorm(a)
 # a_est <- matrix(c(2.3, 1.97, 2.44, -1.34, -1.21, 0.0, 0.07, 0.12, 3.10, 1.74), 5, 2)
 # l2(a_est - a)
 # a <- matrix(c(2.20, 2.00, 2.60, 1.60, 1.70, 1.80, 1.80, 1.90, 1.60, 1.70,
@@ -33,7 +51,7 @@ a
 # d <- matrix(c(0.67, 1.09, -0.18, -0.76, 0.5, -0.41, -0.07, 1.15, 0.13, -1.10,
 #               -0.72, -0.14, -1.22, -1.42, -0.36, -1.26, -0.96, -0.09, -0.70, -1.56), 10, 2)
 # d <- matrix(c(1.1, 0.2, 0.4, 0.2, -0.5), J, 1)
-for (t in 1:5) {
+for (t in 1:mc) {
   # TODO: rewrite this portion
   # Generate data
   X <- matrix(rnorm(N * K), N, K) # Factor score matrix: N x K #nolint
@@ -48,20 +66,31 @@ for (t in 1:5) {
   # dim(X_tilde)
   Y_tilde <- Y2Ytilde(Y) # M x J
   # Y_tilde
-  # c(Y_tilde)
+  # dim(Y_tilde)
   # TODO: fix bugs
+
   ld_mat <- matrix(0, M * J, J * K)
+  # dim(ld_mat)
   for (j in 1:J) {
     ld_mat[(((j - 1) * M + 1):(j * M)), (((j - 1) * K + 1):(j * K))] <- X_tilde
-    # print(dim(ld_mat[(((j - 1) * M + 1):(j * M)), (((j - 1) * K + 1):(j * K))])) #nolint
+    print(dim(ld_mat[(((j - 1) * M + 1):(j * M)), (((j - 1) * K + 1):(j * K))])) #nolint
   }
-  # head(ld_mat)
-  sl_fit <- glm(c(Y_tilde) ~ ld_mat - 1, family = binomial,
+  # dim(ld_mat)
+  # dim(t(Y_tilde))
+  # c(t(Y_tilde))
+  # c(Y_tilde)
+  sl_fit <- glm(c(t(Y_tilde)) ~ ld_mat - 1, family = binomial,
               control = list(maxit = 10000))
+  # coef(sl_fit)
   sl_fit_coef <- matrix(coef(sl_fit))
   # sl_fit_coef
-  a_trans <- matrix(t(a), byrow = TRUE) #nolint
-  error_surr[t] <- l2(matrix(t(a), byrow = TRUE) - sl_fit_coef)
+  sl_fit_coef_2d <- matrix(sl_fit_coef, nrow = 5, byrow = TRUE)
+  # sl_fit_coef_2d
+  sl_fit_coef_qr <- qr_decomp(t(sl_fit_coef_2d))$R
+  # sl_fit_coef_qr
+  error_surr[t] <- fnorm(a_2d - sl_fit_coef_qr)
+  # fnorm(a_2d - sl_fit_coef_qr)
+  # sinTheta(sl_fit_coef_qr, a_2d)
   # error_surr[t]
   values <- mirt(data.frame(Y), 2, itemtype = "graded", method = "MHRM",
                     technical = list(NCYCLES = 10000), pars = "values")
@@ -73,16 +102,23 @@ for (t in 1:5) {
     values[3 * j, 9] <- FALSE
   }
   values[14, 9] <- TRUE
+  dim(Y)
+  # print(values)
   mirt_fit <- mirt(data.frame(Y), 2, itemtype = "graded", method = "MHRM", TOL = 0.0001, #nolint
                  technical = list(NCYCLES = 10000), pars = values)
   mirt_fit_coef <- simplify2array(matrix(coef(mirt_fit, simplify = TRUE)$items[, 1:2], nrow = J, ncol = K)) #nolint
-  error_mirt[t] <- l2(mirt_fit_coef - a)
+  # mirt_fit_coef
+  mirt_fit_coef_qr <- qr_decomp(t(mirt_fit_coef))$R
+  # mirt_fit_coef_qr
+  # fnorm(mirt_fit_coef_qr - a)
+  # sinTheta(mirt_fit_coef, a)
+  error_mirt[t] <- fnorm(mirt_fit_coef_qr - a)
 }
 print("Surrogate Loss MLE:")
-sur_ls <- round(mean(error_surr), 6)
+sur_ls <- round(error_surr / mc, 6)
 print(sur_ls)
 print("CMIRT:")
-cmirt <- round(mean(error_mirt), 6)
+cmirt <- round(error_mirt / mc, 6)
 print(cmirt)
 print(cmirt - sur_ls)
 d_fit2
