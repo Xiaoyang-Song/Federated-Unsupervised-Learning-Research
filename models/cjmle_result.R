d9dac34c5778e7776f22b8f68ba79771061617ec
xysong <xysong25@gmail.com>
2022-10-03 22:42:51 -0400

    Sync experimental results

--- models/cjmle_result.R
+++ models/cjmle_result.R
@@ -80,15 +80,15 @@ plot_fix_N_regime <- function(j) {
   # Extract data from existing .rds data
   for (idx in 1:len_j) {
     # print(n[idx])
-    rds <- readRDS(paste("checkpoint/Fix-N[500]/Fix-N[cc=2][J=", j[idx], "].rds", sep = ""))
+    rds <- readRDS(paste("checkpoint/Fix-N[1000][Prod]/Fix-N[cc=2][J=", j[idx], "].rds", sep = ""))
     cjmle_A[idx] <- mean(rds["cjmle_err_A"])
     cjmle_d[idx] <- mean(rds["cjmle_err_d"])
     cjmle_theta[idx] <- mean(rds["cjmle_err_theta"])
-    # cjmle_prod[idx] <- mean(rds["cjmle_err_prod"])
+    cjmle_prod[idx] <- mean(rds["cjmle_err_prod"])
     mhrm_A[idx] <- mean(rds["mhrm_err_A"])
     mhrm_d[idx] <- mean(rds["mhrm_err_d"])
     mhrm_theta <- mean(rds["mhrm_err_theta"])
-    # mhrm_prod[idx] <- mean(rds["mhrm_err_prod"])
+    mhrm_prod[idx] <- mean(rds["mhrm_err_prod"])
   }
   ic_green <- "#00b384"
   amber <- "#FFBF00"
@@ -103,7 +103,7 @@ plot_fix_N_regime <- function(j) {
     labs(x = "Number of Items", y = "SinTheta Distances") +
     ggtitle("SinTheta Distance vs. Number of Items") +
     easy_center_title()
-  ggsave("Fix-N[500][A].png")
+  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][A].png")
 
   # Plot cjmle & mirt intercept estimator
   ggplot(NULL, aes(x = j)) +
@@ -116,7 +116,7 @@ plot_fix_N_regime <- function(j) {
     labs(x = "Number of Items", y = "FNorm") +
     ggtitle("F-Norm vs. Number of Items") +
     easy_center_title()
-  ggsave("Fix-N[500][d].png")
+  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][d].png")
 
   ggplot(NULL, aes(x = j)) +
     geom_line(aes(y = cjmle_theta, colour = "CJMLE"), size = 1) +
@@ -128,9 +128,19 @@ plot_fix_N_regime <- function(j) {
     labs(x = "Number of Items", y = "SinTheta Distance") +
     ggtitle("SinTheta Distance vs. Number of Items") +
     easy_center_title()
-  ggsave("Fix-N[500][theta].png")
+  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][theta].png")
 
-
+  ggplot(NULL, aes(x = j)) +
+    geom_line(aes(y = cjmle_prod, colour = "CJMLE"), size = 1) +
+    geom_line(aes(y = mhrm_prod, colour = "MHRM"), size = 1) +
+    geom_point(aes(y = cjmle_prod), shape = 0, size = 2.5, color = ic_green) +
+    geom_point(aes(y = mhrm_prod), shape = 0, size = 2.5, color = amber) +
+    scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
+    scale_x_continuous(breaks = j) +
+    labs(x = "Number of Items", y = "SinTheta Distance") +
+    ggtitle("SinTheta Distance vs. Global Sample Size") +
+    easy_center_title()
+  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][Product].png")
 
   return(0)
 }
