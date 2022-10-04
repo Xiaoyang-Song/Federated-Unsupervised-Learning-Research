6dfe7af068001cad4eadaaf648370abaad48a0da
xysong <xysong25@gmail.com>
2022-10-04 16:45:40 -0400

    Sync simluation results

--- models/cjmle_result.R
+++ models/cjmle_result.R
@@ -92,18 +92,21 @@ plot_fix_N_regime <- function(j) {
   len_j <- length(j)
   cjmle_A <- cjmle_d <- cjmle_theta <- cjmle_prod <- rep(0, len_j)
   mhrm_A <- mhrm_d <- mhrm_prod <- mhrm_theta <- rep(0, len_j)
+  mhrm_err_prod_f <- cjmle_err_prod_f <- rep(0, len_j)
   # Extract data from existing .rds data
   for (idx in 1:len_j) {
     # print(n[idx])
-    rds <- readRDS(paste("checkpoint/Fix-N[1000][Prod]/Fix-N[cc=2][J=", j[idx], "].rds", sep = ""))
+    rds <- readRDS(paste("checkpoint/Fix-N[1000]/Fix-N[cc=2][J=", j[idx], "].rds", sep = ""))
     cjmle_A[idx] <- mean(rds["cjmle_err_A"])
     cjmle_d[idx] <- mean(rds["cjmle_err_d"])
     cjmle_theta[idx] <- mean(rds["cjmle_err_theta"])
     cjmle_prod[idx] <- mean(rds["cjmle_err_prod"])
+    cjmle_err_prod_f[idx] <- mean(rds["cjmle_err_prod_f"])
     mhrm_A[idx] <- mean(rds["mhrm_err_A"])
     mhrm_d[idx] <- mean(rds["mhrm_err_d"])
     mhrm_theta <- mean(rds["mhrm_err_theta"])
     mhrm_prod[idx] <- mean(rds["mhrm_err_prod"])
+    mhrm_err_prod_f[idx] <- mean(rds["mhrm_err_prod_f"])
   }
   ic_green <- "#00b384"
   amber <- "#FFBF00"
@@ -116,9 +119,9 @@ plot_fix_N_regime <- function(j) {
     scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
     scale_x_continuous(breaks = j) +
     labs(x = "Number of Items", y = "SinTheta Distances") +
-    ggtitle("SinTheta Distance vs. Number of Items") +
+    ggtitle("SinTheta Distance vs. Number of Items [A]") +
     easy_center_title()
-  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][A].png")
+  ggsave("checkpoint/Fix-N[1000]/plot/Fix-N[1000][A].png")
 
   # Plot cjmle & mirt intercept estimator
   ggplot(NULL, aes(x = j)) +
@@ -129,9 +132,9 @@ plot_fix_N_regime <- function(j) {
     scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
     scale_x_continuous(breaks = j) +
     labs(x = "Number of Items", y = "FNorm") +
-    ggtitle("F-Norm vs. Number of Items") +
+    ggtitle("F-Norm vs. Number of Items [d]") +
     easy_center_title()
-  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][d].png")
+  ggsave("checkpoint/Fix-N[1000]/plot/Fix-N[1000][d].png")
 
   ggplot(NULL, aes(x = j)) +
     geom_line(aes(y = cjmle_theta, colour = "CJMLE"), size = 1) +
@@ -141,9 +144,21 @@ plot_fix_N_regime <- function(j) {
     scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
     scale_x_continuous(breaks = j) +
     labs(x = "Number of Items", y = "SinTheta Distance") +
-    ggtitle("SinTheta Distance vs. Number of Items") +
+    ggtitle("SinTheta Distance vs. Number of Items [Theta]") +
     easy_center_title()
-  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][theta].png")
+  ggsave("checkpoint/Fix-N[1000]/plot/Fix-N[1000][theta].png")
+
+  ggplot(NULL, aes(x = j)) +
+    geom_line(aes(y = cjmle_err_prod_f, colour = "CJMLE"), size = 1) +
+    geom_line(aes(y = mhrm_err_prod_f, colour = "MHRM"), size = 1) +
+    geom_point(aes(y = cjmle_err_prod_f), shape = 0, size = 2.5, color = ic_green) +
+    geom_point(aes(y = mhrm_err_prod_f), shape = 0, size = 2.5, color = amber) +
+    scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
+    scale_x_continuous(breaks = j) +
+    labs(x = "Sample Size", y = "Normalized F-norm") +
+    ggtitle("Normalized F-norm vs. Sample Size [Product]") +
+    easy_center_title()
+  ggsave("checkpoint/Fix-N[1000]/plot/Fix-N[Product][F].png")
 
   ggplot(NULL, aes(x = j)) +
     geom_line(aes(y = cjmle_prod, colour = "CJMLE"), size = 1) +
@@ -153,17 +168,17 @@ plot_fix_N_regime <- function(j) {
     scale_color_manual(name = "Estimator", values = c("CJMLE" = ic_green, "MHRM" = amber)) +
     scale_x_continuous(breaks = j) +
     labs(x = "Number of Items", y = "SinTheta Distance") +
-    ggtitle("SinTheta Distance vs. Global Sample Size") +
+    ggtitle("SinTheta Distance vs. Number of Items [Product]") +
     easy_center_title()
-  ggsave("checkpoint/Fix-N[1000][Prod]/Fix-N[1000][Product].png")
+  ggsave("checkpoint/Fix-N[1000]/plot/Fix-N[1000][Product].png")
 
   return(0)
 }
 
-N <- c(50, 100, 200, 500, 750, 1000, 1500, 2000, 2500)
-# J <- c(5, 10, 15, 20, 25, 30) 
-plot_fix_J_regime(N)
-# plot_fix_N_regime(J)
+# N <- c(50, 100, 200, 500, 750, 1000, 1500, 2000, 2500)
+J <- c(5, 10, 15, 20, 25, 30, 40) 
+# plot_fix_J_regime(N)
+plot_fix_N_regime(J)
 
 # rds <- readRDS("checkpoint/Fix-J[10][Prod]/Fix-J[cc=2][N=1000].rds")
 # rds[" mhrm_err_theta "]
